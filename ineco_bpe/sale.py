b9f92dcf133c64c7e79efcd02e20c8cc46d818b0
Tititab Srisookco <thitithup@gmail.com>
2017-03-22 11:22:28 +0700

    แก้เลขที่ Quotation  ได้

--- ineco_bpe/sale.py
+++ ineco_bpe/sale.py
@@ -50,8 +50,9 @@ class sale_order(osv.osv):
 
     _columns = {
         'name': fields.char('Order Reference', required=True, copy=False,
-                            readonly=False, #states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
-                            select=True,track_visibility='onchange'),
+                            size=32,
+                            readonly=False,
+                            select=True, track_visibility='onchange'),
         'origin': fields.text('Source Document', help="Reference of the document that generated this sales order request."),
         'rev': fields.char('Revision', size=32, required=True),
         'subject': fields.char('Subject', size=254, required=True),
